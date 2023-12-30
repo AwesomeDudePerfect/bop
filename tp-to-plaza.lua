a653acde2c0ffdb3f5d950906125200c5fa7b342
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2023-12-30 15:03:58 +0800

    Add files via upload

---
+++ tp-to-plaza.lua
@@ -0,0 +1,50 @@
+repeat wait() until game:IsLoaded()
+
+if game.placeId ~= 8737899170 or game.placeId ~= 15502339080 or game.placeId ~= 15588442388 then wait(9e9) end
+
+print('executed')
+
+local function serverHop(id)
+	local deep
+	local HttpService = game:GetService("HttpService")
+	local TeleportService = game:GetService("TeleportService")
+	local Players = game:GetService("Players")
+	local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s"
+	local req = request({
+		Url = string.format(sfUrl, id, "Desc", 100)
+	})
+	local body = HttpService:JSONDecode(req.Body)
+	if id == 15502339080 then
+		deep = math.random(2, 4)
+	else
+		deep = 1
+	end
+	
+	if deep > 1 then
+		for i = 1, deep, 1 do
+			req = request({
+				Url = string.format(sfUrl .. "&cursor=" ..body.nextPageCursor, id, "Desc", 100)
+			})
+			body = HttpService:JSONDecode(req.Body)
+			task.wait(0.1)
+		end
+	end
+	task.wait(0.1)
+	local servers = {}
+	if body and body.data then
+		for i, v in next, body.data do
+			if type(v) == "table" and v.playing >= 35 and v.id ~= game.JobId then
+				table.insert(servers, 1, v.id)
+			end
+		end
+	end
+	local randomCount = #servers
+	if not randomCount then
+		randomCount = 2
+	end
+	TeleportService:TeleportToPlaceInstance(id, servers[math.random(1, randomCount)], Players.LocalPlayer)
+end
+
+while wait(1) do
+	serverHop(place)
+end
