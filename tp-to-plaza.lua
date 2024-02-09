1066ddbc527d96dcdf7195c378c1cc9a1244f3c1
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-02-09 22:30:50 +0800

    Update tp-to-plaza.lua

--- tp-to-plaza.lua
+++ tp-to-plaza.lua
@@ -29,21 +29,11 @@ local function serverHop(id)
 		Url = string.format(sfUrl, id, "Desc", 100)
 	})
 	local body = HttpService:JSONDecode(req.Body)
-	if id == 15502339080 then
-		deep = math.random(1, 4)
-	else
-		deep = 1
-	end
-	
-	if deep > 1 then
-		for i = 1, deep, 1 do
-			req = request({
-				Url = string.format(sfUrl .. "&cursor=" ..body.nextPageCursor, id, "Desc", 100)
-			})
-			body = HttpService:JSONDecode(req.Body)
-			task.wait(0.1)
-		end
-	end
+	task.wait(0.2)
+	req = request({
+		Url = string.format(sfUrl .. "&cursor=" ..body.nextPageCursor, id, "Desc", 100)
+	})
+	body = HttpService:JSONDecode(req.Body)
 	task.wait(0.1)
 	local servers = {}
 	if body and body.data then
